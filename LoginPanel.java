9e3b71b88925540f949a26a148445c8108b28999
Tzvetan99 <142295018+Tzvetan99@users.noreply.github.com>
2024-01-12 17:38:39 +0200

    Update LoginPanel.java

--- LoginPanel.java
+++ LoginPanel.java
@@ -4,17 +4,28 @@ import java.awt.*;
 public class LoginPanel extends JPanel {
 
     public LoginPanel(JFrame frame, CardLayout cardLayout, JPanel cards) {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setLayout(null);
+        setSize(800, 150);
 
-        JTextField emailField = new JTextField(20);
-        JPasswordField passwordField = new JPasswordField(20);
+
+        JTextField emailField = new JTextField();
+//        emailField.setPreferredSize(new Dimension(200, 30));
+        JPasswordField passwordField = new JPasswordField( 10);
         JButton loginButton = new JButton("Log In");
 
-        add(new JLabel("Email:"));
+        JLabel emailLbl = new JLabel("Email:");
+        JLabel pswLbl = new JLabel("Password:");
+
+        emailLbl.setBounds(20,50,80,40);
+        emailField.setBounds(120,50,200,40);
+        pswLbl.setBounds(20,100,80,40);
+        passwordField.setBounds(120,100,200,40);
+        loginButton.setBounds(100,180,100,40);
+
+        add(emailLbl);
         add(emailField);
-        add(new JLabel("Password:"));
+        add(pswLbl);
         add(passwordField);
-        add(Box.createVerticalStrut(10));
         add(loginButton);
 
         loginButton.addActionListener(e -> cardLayout.show(cards, "ProductCreationPanel"));
